fe7ee1006ce1d4a9b9618d1712cfad42fa8955d4
Krixx <161109688+Krixx-XD@users.noreply.github.com>
2024-11-11 15:06:36 +0500

    Create vmhash.lua

---
+++ vmhash.lua
@@ -0,0 +1,281 @@
+12u31u923u2391u231u3u912u39u923
+  37d2h
+2 ed
+2
+d2e
+d2d
+e2
+ 2/;ede]]]]]]]
+;de [2;/2 /
+2 ed]/;2
+
+];[e 2;;/]
+/;2 ]ee d
+/];2e d];[e d];
+e d/]
+2;/
+] ed;]
+/e d;]2
+e d;/]
+e 
+/];e d;e2 d;/]2e d;]/e22][e2 d/]
+[e d2]/
+[e ];
+/e ;/]
+ 2
+;/]
+3.a;rf;a.;a
+;.'3r
+.';a3
+;.'3arf;
+'3a.rf'.
+a3r.;a
+'rf3.;a
+'3ra
+3.;'ra3r
+.;'a3r
+.';a3rf
+'.;a3fr
+.;'a3r.;
+a'3a.r
+f;'ar3f.
+;ar3f'.;
+ar3'
+.;3ar'
+;r3.'
+;ra.'
+;rar
+a.';f33.r;'
+3arf.
+';a3rf.
+;'r3fa.';
+rfa3.';
+far.';
+far3
+.';afr3fa.'r;3
+far3.'arf
+.';rfa.';
+afr
+.';ra3f;.r
+.';.
+;'3rfa.
+;'arf;.
+'far;.
+'far3.;
+';
+.afrarf
+;.'raf
+.;'arf3.
+;'ra3ra.;
+3'a3
+.;'r3
+.;r.;rf;.fr.;'rf.r;
+fa'fr3a.
+;'fra.;
+'far.
+;'r3af.;
+'rafra.;'
+r3fa;
+.'r3f.;r.;r
+f'.;
+'.;
+'a3fr
+'.rfa;.
+far3;.
+3fra.;f3arf;.
+;'3rfr3rf.3rf;.3r;
+'3.rf';r3.f';.;.';.'.'/;][;/.]'/[;.
+'[;.';[.
+'[.;
+';./[.;'
+/[;./[.;]'/;.]'/[.;][/.'[.'
+[.;'[.[].'['.][.]'
+;.[].;.;].]';
+/.[.;'.;]/'
+/].
+/'].[';/
+/[.';/[''[.;
+']/.[;
+][.;'[.;];.;['
+7814b616nb78x34867-1m7348x190x8134-38373314-1348-2e-xm0333333333333382304-140mu1x
+;.[;..;.[;[;.
+
+[;
+[.;
+[.;
+[.;
+[.;.]
+/']
+]/'
+]/''/]
+']/
+]/]'/]'/]'/]'/
+]'/]'/]'/]'/
+]'/
+/]']c/'rfc/fr]/'
+
+v]/gv/
+]'
+
+PCRP
+CPPRFFP
+p
+p
+p
+p
+cPRPp
+p
+
+p
+pp
+p
+p
+p
+p
+
+p
+p
+p
+p
+p
+
+p
+pp 
+p
+rp
+fPCP
+rc
+Rp
+fcp
+RPF
+cP
+FCP
+RFCP
+PRCF
+CFPRp
+rcp
+rp
+cp
+rc "ERF"EC'efC??CFE?C?"E?"C?C/'CFE/'CFECFE/'CFE"cFE?"cfE
+
+whEDed
+WE
+
+wd
+w
+de
+wd
+ PUPUPPUPP
+?R?{WD:?W:?R:R?:{{R:?
+      :?R
+      /;[r
+      /;rW
+      ;/rFW
+      r;/r;FW?
+      [rWF
+      /[;rFW{:?
+        rwF[;/rwF[/wr
+        ]wr/]rF]/[w;
+        rF][/r;
+        rFW}?{:
+        WR}/[;
+      rW];
+      /[rFW[];/
+      WR]
+?{:rRW
+        /;[rWF/];[
+        WR[];/r
+        fwr
+        /[];r;FW
+        []wrF/
+        [];FWR
+        /;[]rFW;
+        f/[rwr
+        /[w;/[;
+        ]rwF/[;
+        ]wr/;
+        [w]rwr?:}{wR?{}:
+        wrr
+        /[];rw
+        ]rWr}{W:
+        /rW
+        /[];w
+        /[];rW[;
+        /]rW];
+        /[r;/
+        []rr/:
+        []r
+        /[];
+
+        ]
+E{
+
+          98um39827-2x3-0k2092x-r02rxk7-09230x4-20934x02394x-02x304k209
+          @#U(@*D9u3du93d@u9d#u9m#Du9m89md#@u92#U92u322u8mud38m8mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmQD#qDM#
+m3
+            q3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+             d3h3hqd
+            um 
+            u3 hdm3 dq
+            uh3 qdu
+            h3dq 
+            h3d qhq 
+            d3 hq3
+             qd3
+            qdh3d
+            hn u
+            hnd u
+            hnq3 du
+            hn3d u3
+            hd q
+            h3 dq3qd u
+            3d hqnuhn3q duh q3duh3 dq3 dn 3qdunh
+Q3 duhn3duhd h3 dqu3 dq3 dq3 uhd3 qduh3q d
+            uhn3q d3 qduhq q3dnv
+            gt 
+            V t
+            tv g
+            VT g
+            v tg
+            vt g
+VT
+            vtg
+            tvtv
+            vt
+            tv
+            ttvg
+            V T
+            t
+            v T v
+            tv
+            tgv
+            T
+            v T 
+            t / t 
+            /t
+            / t
+            / t t/ tg /
+
+            t 
+            t
+
+            t 
+            
